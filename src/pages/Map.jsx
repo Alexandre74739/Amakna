b353b01c5fd043ab787cf36a9e7b741c32b49ef2
perez <perezalexandre430@gmail.com>
2025-12-03 14:20:11 +0100

    ajout home HTML

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,8 +1,12 @@
+import pinpin from '../assets/pinpin.png';
+import './Map.scss';
+
 function Map() {
   return (
     <div>
       <h1>Amakna</h1>
       <p>Plongez dans le monde du Krosmoz</p>
+      <img src={pinpin} alt="Logo" />
     </div>
   );
 }
