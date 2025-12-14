b64feca214ccb495ac535069a0c2c811c97166a1
perez <perezalexandre430@gmail.com>
2025-12-14 14:27:02 +0100

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import logo from '../assets/logo.png';
+import burger from '../assets/menu-hamburger.png';
 import './Header.scss';
 
 function Header() {
+  const [open, setOpen] = useState(false);
+
   return (
     <header>
       <img src={logo} alt="Logo" />
@@ -12,6 +16,22 @@ function Header() {
         <Link to="/encyclopedie">Encyclopédie</Link>
         <Link to="/jeux">Mini-Jeux</Link>
       </nav>
+
+      {/* Responsive */}
+      <img
+        src={burger}
+        alt="Menu"
+        className="burger"
+        onClick={() => setOpen(!open)}
+      />
+
+      {open && (
+        <div className="nav-mobile">
+          <Link to="/" onClick={() => setOpen(false)}>Map Monde</Link>
+          <Link to="/encyclopedie" onClick={() => setOpen(false)}>Encyclopédie</Link>
+          <Link to="/jeux" onClick={() => setOpen(false)}>Mini-Jeux</Link>
+        </div>
+      )}
     </header>
   );
 }
